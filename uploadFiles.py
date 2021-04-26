b141d9af8971038bdd560cddc7dde45ad69a136b
Sravya <65549677+Sravya2007@users.noreply.github.com>
2021-04-26 20:58:32 +0530

    Uploading files to Dropbox

---
+++ uploadFiles.py
@@ -0,0 +1,35 @@
+import dropbox
+import os
+
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        #initializing dropbox
+        dbx = dropbox.Dropbox(self.access_token)
+
+        for root, dirs, files in os.walk(file_from):
+            #for each file in files list
+            for file_name in files:
+                #full path
+                local_path = os.path.join(root, file_name)
+                relative_path = os.path.relpath(local_path, file_from)
+                dropbox_path = os.path.join(file_to, relative_path)
+
+                with open(local_path, rb) as f:
+                    dbx.files_upload(f.read(), dropbox_path, mode = WriteMode('overwrite'))
+
+def main():
+    access_token = 'xlCQ0Lsv2rwAAAAAAAAAARAqqtJrRPcMMuW-Mw3kLij3ZLDBrhBEf7gG4e2PssIc'
+    transferData = TransferData(access_token)
+
+    file_from = input("Enter the file name to transfer: ")
+    #/folder_name_in_dropbox/filename.ext
+    file_to = input("Enter the destination where the file should be transferred: ")
+
+    transferData.upload_file(file_from, file_to)
+    print("File uploaded!")
+
+if __name__ == '__main__':
+    main()
